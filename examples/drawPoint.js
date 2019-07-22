54128bd6a152c6460840e7c03c4ae1cde131e98c
penghui wu <penghui@11deMacBook-Pro.local>
2019-07-22 18:59:54 +0800

    添加注释

--- examples/drawPoint.js
+++ examples/drawPoint.js
@@ -13,42 +13,45 @@ const FSHADER_SOURCE = `
 
 function main() {
     let canvas = document.getElementById('webGL');
+    //获取webGL上下文
     let gl = canvas.getContext("webgl");
-    // debugger;
+    //创建着色器对象
     const shaderProgram = initShader(gl, VSHADER_SOURCE, FSHADER_SOURCE);
+    //验证着色器对象
     if(!shaderProgram) {
         console.log('shderProgram filed ', shaderProgram)
     }
+    //使用着色器对象渲染
     gl.useProgram(shaderProgram);
-    gl.program = shaderProgram;
-
+    //指定背景色
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
-
+    //清空绘图区域
     gl.clear(gl.COLOR_BUFFER_BIT);
-
+    //绘制图形
     gl.drawArrays(gl.POINTS, 0, 1)
-
-    console.log('draw over')
 }
 
 
 
 function initShader(gl, vsSource, fsSource) {
+    //加载着色器对象
     const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
     const fragmentShder = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
 
-    //创建着色器程序
+    //验证是否加载成功
     if(!vertexShader || !fragmentShder) {
         return null;
     }
+    //创建着色器程序
     const shaderProgram = gl.createProgram();
     if(!shaderProgram) {
         console.log('program is null')
         return null;
     }
+    //为着色器程序附加着色器对象
     gl.attachShader(shaderProgram, vertexShader);
     gl.attachShader(shaderProgram, fragmentShder);
-
+    //连接一个着色程序与着色器
     gl.linkProgram(shaderProgram);
     let linked = gl.getProgramParameter( shaderProgram, gl.LINK_STATUS);
     if(!linked) {
@@ -63,21 +66,20 @@ function initShader(gl, vsSource, fsSource) {
 
 
 //创建指定类型的着色器， 上传source源码并编译
-
 function loadShader(gl, type, source) {
+    //创建着色器对象
      const shader = gl.createShader(type);
+     //将源代码传给着色器对象
      gl.shaderSource(shader, source);
+     //编译着色器对象
      gl.compileShader(shader);
-
+    //验证着色器对象是否编译成功
      const compiled = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
-
      if(!compiled) {
          let error = gl.getShaderInfoLog(shader);
          console.log('failed to compile shader', error);
          gl.deleteShader(shader);
-
          return null;
      }
-
      return shader;
 }
